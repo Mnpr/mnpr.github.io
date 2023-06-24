02d7bf77175e4afee3ef48dbf547a8ab92594ef9
Mnpr <acharya.sudesh366@gmail.com>
2023-06-24 12:09:59 +0200

    finalized reusable components

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,23 +1,31 @@
 <template>
-    <div>
-        <div>
-            <ul>
-                <li>
-                    <Icon name="uil:github"/>
-                </li>
-                <li>
-                    <Icon name="uil:linkedin"/>
-                </li>
-                <li>
-                    <Icon name="uil:instagram"/>
-                </li>
-                <li>
-                    <Icon name="tabler:brand-soundcloud"/>
-                </li>
-                <li>
-                    <Icon name="uil:youtube"/>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <footer>
+        <ul>
+            <li>
+                <a href="https://github.com/Mnpr" target="_blank">
+                    <UIcon name="i-heroicons-code-bracket" />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/sud3sh/" target="_blank">
+                    <UIcon name="i-heroicons-briefcase"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://unsplash.com/@sud3sh_" target="_blank">
+                    <UIcon name="i-heroicons-photo"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/@sud3sh_" target="_blank">
+                    <UIcon name="i-heroicons-video-camera"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://soundcloud.com/xpectron" target="_blank">
+                    <UIcon name="i-heroicons-musical-note"/>
+                </a>
+            </li>
+        </ul>
+    </footer>
 </template>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,23 +1,46 @@
+<script setup>
+const colorMode = useColorMode()
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark'
+    },
+    set() {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+})
+</script>
+
 <template>
-    <div>
-        <div>
+    <header>
+        <nav>
             <ul>
-                <li><Icon name="uil:home"/></li>
-                <li><Icon name="jam:write" /></li>
-                <li><Icon name="iconamoon:profile-bold"/></li>
                 <li>
-                    <div>
-                        <Icon name="mdi:theme-light-dark"/>
-                        <select v-model="$colorMode.preference">
-                            <option value="system">System</option>
-                            <option value="light">light</option>
-                            <option value="dark">dark</option>
-                            <option value="sepia">sepia</option>
-                        </select>
-                    </div>
+                    <NuxtLink to="/">
+                        <UIcon name="i-heroicons-home" />
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/writes">
+                        <UIcon name="i-heroicons-pencil-square" />
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">
+                        <UIcon name="i-heroicons-user-circle" />
+                    </NuxtLink>
+                </li>
+                <li>
+                    <ClientOnly>
+                        <!-- <UButton :icon="isDark ? 'i-heroicons-moon' : 'i-heroicons-sun'" variant="ghost" aria-label="Theme"
+                            @click="isDark = !isDark" /> -->
+
+                        <template #fallback>
+                            <div class="w-8 h-8" />
+                        </template>
+                    </ClientOnly>
                 </li>
             </ul>
-        </div>
-    </div>
+        </nav>
+    </header>
 </template>
 
