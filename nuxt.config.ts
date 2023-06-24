ae392c33c8ea3851a0b607d3e05103e2243ec2de
Mnpr <acharya.sudesh366@gmail.com>
2023-06-24 12:16:31 +0200

    add nuxtlabs ui and color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
-        '@nuxt/content',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode',
-        'nuxt-icon',
+        '@nuxthq/ui',
     ],
-    colorMode: { classSuffix: '' },
-    content: {
-      // https://content.nuxtjs.org/api/configuration
-    }
+    colorMode: {
+        preference: 'light'
+    },
 })
