67c1efc71244a89281ee2152224f2505d614d78f
Mnpr <acharya.sudesh366@gmail.com>
2023-07-08 11:17:28 +0200

    simplify about

--- pages/about.vue
+++ pages/about.vue
@@ -24,8 +24,8 @@ const links = [{
         </ASection>
         <ASection class="about-block-section">
             <span class="text-2xl text-center">
-                Learning, distilling simplicity out of evergrowing information and persued complexity,
-                turtle and part-time embarks towards wonders of tomorrow O:).
+                Learning and working with Information Science, distilling simplicity out of evergrowing information and
+                persued complexities, turtle and part-time embarks forth towards wonders of tomorrow O:).
             </span>
         </ASection>
         <hr />
