28aae5c0bcfb08faed3a88cf24a6315056c2f043
Mnpr <acharya.sudesh366@gmail.com>
2024-06-23 01:21:57 +0200

    add nav route -1, +1

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -15,6 +15,13 @@ const isDark = computed({
         <footer>
             <hr />
             <span>
+                <UButton
+                    icon="i-heroicons-chevron-double-left-16-solid"  
+                    size="sm"
+                    color="primary"
+                    @click="$router.go(-1)"
+                    variant="ghost"
+                />
                 <ClientOnly>
                     <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" variant="ghost"
                         aria-label="Theme" @click="isDark = !isDark" />
@@ -22,6 +29,13 @@ const isDark = computed({
                         <span class="w-8 h-8" />
                     </template>
                 </ClientOnly>
+                <UButton
+                    icon="i-heroicons-chevron-double-right-16-solid"  
+                    size="sm"
+                    color="primary"
+                    @click="$router.go(+1)"
+                    variant="ghost"
+                />
             </span>
         </footer>
     </AContainer>
